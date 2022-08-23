0f39a49a953e55c5b29c2ce5801fe0e821cd52ee
Pavel Ryabov <pavel.ryabov.zm@gmail.com>
2022-08-23 21:26:57 +1000

    Init

---
+++ src/main/java/xyz/pary/it_one/cup2022final/ItOneCup2022FinalApplication.java
@@ -0,0 +1,13 @@
+package xyz.pary.it_one.cup2022final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItOneCup2022FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItOneCup2022FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/pary/it_one/cup2022final/ItOneCup2022FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.pary.it_one.cup2022final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItOneCup2022FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
